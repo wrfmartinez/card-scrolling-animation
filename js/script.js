954426167467eb1790a7a4f7fcd349adf42a7031
wrfmartinez <wrmartinez709@gmail.com>
2024-04-12 15:57:00 -0400

    Create zoom in effect on page load

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("Hello, world!");
+const circleElement = document.querySelector("div.circle");
+const navElement = document.querySelector("nav");
+const headerElement = document.querySelector("header");
+
+const hideElements = () => {
+  navElement.classList.add("hide");
+  headerElement.classList.add("hide");
+}
+
+const zoomInEffect = () => {
+  circleElement.classList.add("zoom-in");
+}
+
+window.addEventListener("DOMContentLoaded", hideElements);
+window.addEventListener("DOMContentLoaded", setTimeout(() => {
+  zoomInEffect();
+}, 200));
