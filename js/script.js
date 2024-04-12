2cd199ab0cd04f9fafb8c52a712f46d3662c7265
wrfmartinez <wrmartinez709@gmail.com>
2024-04-12 17:40:50 -0400

    Fix load animation along, add text slide up effect

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const circleContainerElement = document.querySelector("div.circle-container");
 const circleElement = document.querySelector("div.circle");
 const navElement = document.querySelector("nav");
 const headerElement = document.querySelector("header");
+const headerDescriptionElement = document.getElementById("header-description");
+console.log(headerDescriptionElement);
 
 const hidePageContent = () => {
   navElement.classList.add("hide");
@@ -12,7 +14,7 @@ const zoomInEffect = () => {
   // Waits 0.2 seconds before initiating zoom-in animation to avoid starting animation too quickly on page load
   setTimeout(() => {
     circleElement.classList.add("zoom-in");
-  }, 200)
+  }, 10)
 }
 
 const restorePageContent = () => {
@@ -23,7 +25,8 @@ const restorePageContent = () => {
 
   setTimeout(() => {
     navElement.classList.replace("transparent", "fade-in");
-  }, 200)
+    headerDescriptionElement.classList.add("slide-up");
+  }, 50)
 }
 
 // Will remove main components on the page first on DOM content load
