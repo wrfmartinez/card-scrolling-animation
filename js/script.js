6d9a5bf3836a26d4fcc7b7ef87586795a7cfea5a
wrfmartinez <wrmartinez709@gmail.com>
2024-04-12 16:43:59 -0400

    Implement restorePageContent function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const circleContainerElement = document.querySelector("div.circle-container");
 const circleElement = document.querySelector("div.circle");
 const navElement = document.querySelector("nav");
 const headerElement = document.querySelector("header");
@@ -8,13 +9,27 @@ const hidePageContent = () => {
 }
 
 const zoomInEffect = () => {
-  circleElement.classList.add("zoom-in");
+  // Waits 0.2 seconds before initiating zoom-in animation to avoid starting animation too quickly on page load
+  setTimeout(() => {
+    circleElement.classList.add("zoom-in");
+  }, 200)
 }
 
-// On page load the page will remove main components on the page first
-// then initiate the zoom-in effect and repopulate the main components onto the page
+const restorePageContent = () => {
+  circleContainerElement.remove();
+  
+  navElement.classList.remove("hide");
+  headerElement.classList.remove("hide");
 
+  setTimeout(() => {
+    navElement.classList.replace("transparent", "fade-in");
+  }, 200)
+}
+
+// Will remove main components on the page first on DOM content load
+// then initiate the zoom-in effect and repopulate the main components onto the page
 window.addEventListener("DOMContentLoaded", hidePageContent);
-window.addEventListener("DOMContentLoaded", setTimeout(() => {
-  zoomInEffect();
-}, 200));
+window.addEventListener("DOMContentLoaded", zoomInEffect);
+setTimeout(() => {
+  restorePageContent();
+}, 500);
