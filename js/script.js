37c797046b469efa17bb8282a5f59a149c20cbda
wrfmartinez <wrmartinez709@gmail.com>
2024-04-13 15:19:14 -0400

    Rough draft of scroll animations based on primary card position

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ const zoomInEffect = () => {
 
 const restorePageContent = () => {
   circleContainerElement.remove();
-  
+
   navElement.classList.remove("hide");
   headerElement.classList.remove("hide");
 
@@ -32,6 +32,54 @@ const restorePageContent = () => {
 }
 
 const switchCard = () => {
+  const rightStyle = getComputedStyle(rightCardToSwitch);
+  const leftStyle = getComputedStyle(leftCardToSwitch);
+  const rightCardZIndexValue = rightStyle.zIndex;
+  const leftCardZIndexValue = leftStyle.zIndex;
+
+  // TODO: THESE CONDITIONS SHOULD FIRE ONLY AT CERTAIN POSITION ON THE WINDOW HEIGHT *NOT* ON EVERY SCROLL EVENT
+
+  // IF RIGHT CARD IS THE PRIMARY CARD THEN SHIFT CARDS
+  if (rightCardZIndexValue === "1") {
+    middleCardToSwitch.style.transform = "translateX(205px)";
+
+    leftCardToSwitch.style.filter = "blur(0)";
+    leftCardToSwitch.style.opacity = "1";
+    leftCardToSwitch.style.paddingTop = "70px";
+    leftCardToSwitch.style.paddingBottom = "0";
+    leftCardToSwitch.style.zIndex = "1";
+    leftCardToSwitch.style.transform = "translateX(205px)";
+
+    rightCardToSwitch.style.opacity = "0.7"
+    rightCardToSwitch.style.filter = "blur(0.28rem)"
+    rightCardToSwitch.style.zIndex = 0;
+    rightCardToSwitch.style.transform = "translateX(-405px)";
+    rightCardToSwitch.style.paddingTop = "165px";
+  }
+
+  // IF LEFT CARD IS THE PRIMARY CARD THEN SET THE ORIGINAL STATE OF CARDS
+  if (leftCardZIndexValue === "1") {
+    middleCardToSwitch.style.zIndex = "1";
+    middleCardToSwitch.style.transform = "translateX(0)";
+    middleCardToSwitch.style.filter = "blur(0)";
+    middleCardToSwitch.style.paddingTop = 0;
+
+    leftCardToSwitch.style.filter = "blur(0.28rem)";
+    leftCardToSwitch.style.opacity = "0.7";
+    leftCardToSwitch.style.paddingTop = "165px";
+    leftCardToSwitch.style.paddingBottom = "50px";
+    leftCardToSwitch.style.zIndex = "0";
+    leftCardToSwitch.style.transform = "translateX(0)";
+
+    rightCardToSwitch.style.opacity = "0.7"
+    rightCardToSwitch.style.filter = "blur(0.28rem)"
+    rightCardToSwitch.style.zIndex = 0;
+    rightCardToSwitch.style.transform = "translateX(0)";
+    rightCardToSwitch.style.paddingTop = "165px";
+    rightCardToSwitch.style.paddingBottom = "50px";
+  }
+
+  // FIRST SHIFT OF CARDS WHERE THE MIDDLE CARD IS THE PRIMARY CARD
   middleCardToSwitch.classList.add("switchMiddleCard");
   leftCardToSwitch.classList.add("switchLeftCard");
   rightCardToSwitch.classList.add("switchRightCard");
