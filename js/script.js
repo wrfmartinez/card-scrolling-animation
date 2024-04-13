1b0d5256122be0a157c58faa8a8ebf4026f8c572
wrfmartinez <wrmartinez709@gmail.com>
2024-04-13 14:40:17 -0400

    Add initial card switch animation function

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const circleElement = document.querySelector("div.circle");
 const navElement = document.querySelector("nav");
 const headerElement = document.querySelector("header");
 const headerDescriptionElement = document.getElementById("header-description");
+const leftCardToSwitch = document.querySelector("#card-left.switchCard");
+const middleCardToSwitch = document.querySelector("#card-middle.switchCard");
+const rightCardToSwitch = document.querySelector("#card-right.switchCard");
 
 const hidePageContent = () => {
   navElement.classList.add("hide");
@@ -28,6 +31,12 @@ const restorePageContent = () => {
   }, 50)
 }
 
+const switchCard = () => {
+  middleCardToSwitch.classList.add("switchMiddleCard");
+  leftCardToSwitch.classList.add("switchLeftCard");
+  rightCardToSwitch.classList.add("switchRightCard");
+}
+
 // Will remove main components on the page first on DOM content load
 // then initiate the zoom-in effect and repopulate the main components onto the page
 window.addEventListener("DOMContentLoaded", hidePageContent);
@@ -35,3 +44,5 @@ window.addEventListener("DOMContentLoaded", zoomInEffect);
 setTimeout(() => {
   restorePageContent();
 }, 500);
+
+window.addEventListener("scroll", switchCard);
